f88dc91de59f5949e58bac0f385502a1893d0e0f
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-04 10:08:20 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,16 @@
+print(len("srinivas"))
+a=("python")
+print(a,type(a))
+print(a[0])
+
+ print("python"[0])
+ print("python"[1])
+ print("python"[2])
+ print("python"[3])
+ print("python"[4])
+ print("python"[5])
+ print("srinivasnaidu"[9])
+ print('sriniasnaidu'[15])
+
+x="delllaptop"
+print(x[1]+x[4]+x[3]+x[2]+x[5]+x[8]+x[6]+x[7]+x[9])
